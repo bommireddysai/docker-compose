90565b89e5365c9012020c1d235f3673c9525de0
sai kumar <ubuntu@ip-172-31-28-121.us-east-2.compute.internal>
2021-10-24 02:38:59 +0000

    restore hello.java file from commit: 86d9f71

--- hello.java
+++ hello.java
@@ -1,8 +1,5 @@
-// this javaprogram will print a sample message
 class Simple{  
 	    public static void main(String args[]){  
 		         System.out.println("Hello Java");  
-	
-         system.out.printin("welcome to spring boot");		 
-	    }  
+			     }  
 } 
